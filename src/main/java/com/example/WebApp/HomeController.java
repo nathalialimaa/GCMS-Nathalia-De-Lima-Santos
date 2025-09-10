7d89eaa95e56a788935519373208711d3f068eaf
Nathalia de Lima <nathylima464@gmail.com>
2025-09-10 19:59:51 -0300

    adicionando arquivos da pratica 2 atraves do git

---
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.WebApp; 
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.ResponseBody; 
+
+@Controller 
+public class HomeController { 
+	
+	@RequestMapping("/") 
+	public @ResponseBody String greeting() { 
+	return "Hello, World!"; 
+	}
+	
+	@RequestMapping("/Nathalia") 
+	 public @ResponseBody String greetingNathalia() { 
+	  return "Hello, Nathalia!"; 
+	 } 
+ 
+}
